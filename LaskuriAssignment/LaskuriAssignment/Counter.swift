ff96a8b14affad9867bbf0ebf2d62530a39ac48c
Samu Pietilä <122264320+Samupietila@users.noreply.github.com>
2025-01-28 17:31:02 +0200

    Add files via upload

---
+++ LaskuriAssignment/LaskuriAssignment/Counter.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+class Counter {
+    var lowerBound: Int
+    var upperBound: Int
+    var step: Int
+    var ops: Int
+    var value: Int
+    
+    init(lowerBound: Int = 0, upperBound: Int = 5, step: Int = 1, ops: Int = 0, value: Int = 0) {
+        self.lowerBound = lowerBound
+        self.upperBound = upperBound
+        self.step = step
+        self.ops = ops
+        self.value = lowerBound
+    }
+    
+    func setStep(_ step: Int) {
+        self.step = if step <= self.upperBound {
+            step
+        } else {
+            1
+        }
+    }
+    
+    func increment() {
+        self.value += self.step
+        if self.value > self.upperBound {
+            self.value = self.lowerBound
+        }
+        }
+    
+    func decrement() {
+        self.ops += 1
+        var erotus = self.value - self.step
+        if self.step < 2 {
+            erotus = 0
+        }
+        self.value -= self.step
+        if self.value < self.lowerBound {
+            self.value = self.upperBound - erotus
+                    }
+        
+    }
+    
+    
+}
