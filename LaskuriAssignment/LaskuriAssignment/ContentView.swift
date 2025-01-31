efcaa2843c9a563d1931a5d7bf95597d1cd48ee2
Samu Pietilä <122264320+Samupietila@users.noreply.github.com>
2025-01-31 13:53:41 +0200

    Add files via upload

--- LaskuriAssignment/LaskuriAssignment/ContentView.swift
+++ LaskuriAssignment/LaskuriAssignment/ContentView.swift
@@ -2,23 +2,47 @@
 //  ContentView.swift
 //  LaskuriAssignment
 //
-//  Created by Peter Hjort työ on 23.1.2025.
+//  Created by Samu Pietilä työ on 31.1.2025.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(Counter.self) private var counter
+    @State private var inputValue: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("CounterApp")
+                .padding()
+                .font(Font.system(size: 50))
+            Text("Counter Value:  \(counter.value)")
+                .padding()
+                .font(Font.system(size: 30))
+            HStack {
+                Button(action: counter.increment){
+                    Text("Increment")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(100)
+                }
+                Button(action: counter.decrement){
+                    Text("Decrement")
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(100)
+                }}
+            TextField("Step: ", text: $inputValue)
+                .padding()
+                .keyboardType(.numberPad)
+                .onSubmit {
+                    counter.setStep(Int(inputValue) ?? counter.step)
+                }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
+        .environment(Counter())
 }
--- LaskuriAssignment/LaskuriAssignment/Counter.swift
+++ LaskuriAssignment/LaskuriAssignment/Counter.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+@Observable
 class Counter {
     var lowerBound: Int
     var upperBound: Int
